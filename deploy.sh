b69a7c2dede8413f4ec312f09f65af73fad641f9
Dudhane Shreyanshu <dudhaneshrey86@gmail.com>
2021-07-09 16:47:23 +0530

    change

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:DudhaneShrey86/android_developer_portfolio.git master:gh-pages
+git push -f git@github.com:DudhaneShrey86/android_developer_portfolio.git main:gh-pages
 cd -
